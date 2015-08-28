9940ed5861a087946d51905e73f8b73846cf3808
kimx <kimxinfo@gmail.com>
2015-08-28 15:13:42 +0800

    init

---
+++ GetSetDataWeb/App/Home/Home.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#set-data-from-selection').click(setDataFromSelection);
+        });
+    };
+
+    function getDataFromSelection() {
+        //取得選取的儲存格資料，Office.CoercionType.Text 指定回傳結果的格式(本例為文字)
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('選取的資料:', '"' + result.value + '"');
+                } else {
+                    app.showNotification('錯誤:', result.error.message);
+                }
+            }
+        );
+    }
+
+    function setDataFromSelection() {
+        Office.context.document.setSelectedDataAsync($("#myValue").val(),null,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('設定結果:', '"' + result.status + '"');
+                } else {
+                    app.showNotification('錯誤:', result.error.message);
+                }
+            }
+        );
+    }
+})();
---
+++ SetDataTableWeb/App/Home/Home.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#set-data-from-selection').click(setDataTable);
+        });
+    };
+
+    // Reads data from current document selection and displays a notification
+    function setDataTable() {
+        var dataTable = new Office.TableData();
+        dataTable.headers = ['Name', 'City'];
+        dataTable.rows = [['Kim', 'Kao'], ['David', 'Tai'], ['John', 'Tan']];
+        Office.context.document.setSelectedDataAsync(dataTable, { CoercionType: Office.CoercionType.Table },
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('The selected text is:', '"' + result.status + '"');
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+})();
